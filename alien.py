d5f1d261f064fd5c468283405e64cc6e89355c10
chashuramen <aka.hjd@gmail.com>
2019-08-29 18:18:49 +0800

    Initial commit.

---
+++ alien.py
@@ -0,0 +1,12 @@
+#! python3
+# alien.py
+"""Class file for aliens"""
+
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    def __init__(self, screen, settings):
+        Sprite.__init__(self)
+        pass
