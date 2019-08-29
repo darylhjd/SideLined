ac05699f25bc8037c3eb2a04620cde9c837db64c
chashuramen <aka.hjd@gmail.com>
2019-08-30 01:29:58 +0800

    Omg it's rain!!!

--- rain.py
+++ rain.py
@@ -2,6 +2,7 @@
 # rain.py
 """Class file for rain effect"""
 
+import random
 import pygame
 from pygame.sprite import Sprite
 
@@ -17,8 +18,36 @@ class Rain(Sprite):
 
         # Properties
         self.color = (230, 230, 230)
-        self.width = 5
+        self.width = 3
         self.height = 15
-        self.rect = pygame.transform.rotozoom(pygame.Rect(0, 0, self.width, self.height), -45, 1)
+        self.surface = pygame.transform.rotozoom(pygame.Surface((self.width, self.height)), -30, 1)
+        self.rect = self.surface.get_rect()
 
         # Position raindrop
+        chance = random.random()
+        if chance <= 0.50:
+            self.rect.bottom = 0
+            self.rect.centerx = random.randint(0, self.screen_rect.right)
+        else:
+            self.rect.left = self.screen_rect.right
+            self.rect.bottom = random.randint(0, self.screen_rect.height)
+
+        self.bottom = float(self.rect.bottom)
+        self.centerx = float(self.rect.centerx)
+
+    def draw_rain(self):
+        self.surface.fill(self.color)
+        self.screen.blit(self.surface, self.rect)
+
+    def update(self):
+        kill_chance = random.random()
+        self.bottom += self.settings.rainy_move
+        self.centerx += self.settings.rainx_move
+
+        if self.rect.top >= self.screen_rect.bottom or self.rect.left <= 0 or kill_chance <= 0.001:
+            self.kill()
+
+        self.rect.bottom = self.bottom
+        self.rect.centerx = self.centerx
+
+        self.draw_rain()
