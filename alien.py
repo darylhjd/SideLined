e9e43cebffc091669fa7666b69dae1ff83690805
chashuramen <aka.hjd@gmail.com>
2019-08-30 00:14:54 +0800

    More attributes cos aliens aren't boring.

--- alien.py
+++ alien.py
@@ -9,4 +9,10 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     def __init__(self, screen, settings):
         Sprite.__init__(self)
-        pass
+        self.settings = settings
+
+        # Screen settings
+        self.screen = screen
+
+        # Image
+        self.image = pygame.transform.rotozoom(pygame.image.load(r"image/ufo.bmp"), 0, 0.2)
