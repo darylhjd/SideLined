16459dc4b0c17c1796703aacf3f1eb7d39e48bc6
chashuramen <aka.hjd@gmail.com>
2019-08-30 14:55:29 +0800

    Refactor ship movement and blit background.

--- gamefunctions.py
+++ gamefunctions.py
@@ -8,32 +8,38 @@ from bullet import Bullet
 from rain import Rain
 
 
-def when_keyup(ship, event):
+def ship_movement(ship, event, boolean):
+    if event.key == pygame.K_DOWN:
+        ship.move_down = boolean
+
+    if event.key == pygame.K_UP:
+        ship.move_up = boolean
+
     if event.key == pygame.K_LEFT:
-        ship.move_down = False
+        ship.move_left = boolean
 
     if event.key == pygame.K_RIGHT:
-        ship.move_up = False
+        ship.move_right = boolean
+
+
+def when_keyup(ship, event):
+    ship_movement(ship, event, False)
 
 
 def when_keydown(ship, event):
     if event.key == pygame.K_q:
         sys.exit()
 
-    if event.key == pygame.K_LEFT:
-        ship.move_down = True
-
-    if event.key == pygame.K_RIGHT:
-        ship.move_up = True
+    ship_movement(ship, event, True)
 
 
 def auto_shooting(screen, settings, ship, bullets):
     if pygame.key.get_pressed()[pygame.K_z]:
-        settings.currentbullet_interval += 1
-
-        if settings.currentbullet_interval == settings.bullet_interval:
+        if settings.currentsb_interval == settings.sb_interval:
             bullets.add(Bullet(screen, settings, ship))
-            settings.currentbullet_interval = 0
+            settings.currentsb_interval = 0
+
+        settings.currentsb_interval += 1
 
 
 def check_events(screen, settings, ship, bullets):
@@ -51,20 +57,18 @@ def check_events(screen, settings, ship, bullets):
 
 
 def create_rain(screen, settings, rains):
-    settings.currentrain_interval += 1
-
     if settings.currentrain_interval == settings.rain_interval:
         rains.add(Rain(screen, settings))
         settings.currentrain_interval = 0
 
+    settings.currentrain_interval += 1
 
-def update_screen(screen, settings, ship, bullets, rains):
-    screen.fill(settings.bgcolor)
+
+def update_screen(screen, settings, ship, bullets, rains, bg_image):
+    screen.blit(bg_image, (0, 0))
 
     bullets.update()
-
-    ship.move()
-    ship.blitme()
+    ship.update()
 
     create_rain(screen, settings, rains)
     pygame.sprite.spritecollide(ship, rains, dokill=True, collided=pygame.sprite.collide_mask)
