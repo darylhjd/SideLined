068e82e9a110744d8fa3c15c22053227df447d32
chashuramen <aka.hjd@gmail.com>
2019-08-30 09:48:42 +0800

    More equal rain generation

--- rain.py
+++ rain.py
@@ -17,18 +17,21 @@ class Rain(Sprite):
         self.screen_rect = self.screen.get_rect()
 
         # Properties
+        self.kill_chance = 0.001
         self.color = (230, 230, 230)
         self.width = 3
         self.height = 15
-        self.surface = pygame.transform.rotozoom(pygame.Surface((self.width, self.height)), -30, 1)
+        self.surface = pygame.transform.rotate(pygame.Surface((self.width, self.height)), -30)
         self.rect = self.surface.get_rect()
 
         # Position raindrop
         chance = random.random()
-        if chance <= 0.50:
+        if chance <= 0.60:
+            # Creates rain from the top of the screen
             self.rect.bottom = 0
             self.rect.centerx = random.randint(0, self.screen_rect.right)
         else:
+            # Creates rain from the right of the screen.
             self.rect.left = self.screen_rect.right
             self.rect.bottom = random.randint(0, self.screen_rect.height)
 
@@ -40,11 +43,11 @@ class Rain(Sprite):
         self.screen.blit(self.surface, self.rect)
 
     def update(self):
-        kill_chance = random.random()
+        chance = random.random()
         self.bottom += self.settings.rainy_move
         self.centerx += self.settings.rainx_move
 
-        if self.rect.top >= self.screen_rect.bottom or self.rect.left <= 0 or kill_chance <= 0.001:
+        if self.rect.top >= self.screen_rect.bottom or self.rect.left <= 0 or chance <= self.kill_chance:
             self.kill()
 
         self.rect.bottom = self.bottom
