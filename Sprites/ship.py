2504bc8b2456beecbe87339e32d759af89f4ca67
chashuramen <aka.hjd@gmail.com>
2019-09-01 21:50:36 +0800

    Some changes

--- Sprites/ship.py
+++ Sprites/ship.py
@@ -34,9 +34,10 @@ class Ship:
         self.move_right = False
 
         # Small hitbox image
-        self.hb_image = pygame.transform.rotozoom(pygame.image.load(r"images/better_hitbox.bmp"),
-                                                  0, 0.03)
+        self.hb_image = pygame.transform.rotozoom(pygame.image.load(r"images/better_hitbox.bmp").convert_alpha(),
+                                                  0, 0.015)
         self.hb_mask = pygame.mask.from_surface(self.hb_image)
+        self.hb_mask_rect = self.hb_mask.get_rect()
 
         # Position hitbox
         self.hb_rect = self.hb_image.get_rect()
@@ -46,13 +47,13 @@ class Ship:
         # Hitbox flags
         self.show_hitbox = False
 
-    def update_mask(self):
-        self.mask = self.hb_mask if self.show_hitbox else self.ship_mask
-
     def update_hitbox(self):
-        self.update_mask()
         if self.show_hitbox:
+            self.hb_mask_rect.center = (self.hb_centerx, self.hb_centery)
             self.screen.blit(self.hb_image, self.hb_rect)
+            self.mask = self.hb_mask
+        else:
+            self.mask = self.ship_mask
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
@@ -60,24 +61,22 @@ class Ship:
     def move(self):
         if self.move_up and self.rect.top > 0:
             self.centery -= self.ymove
-            self.hb_centery -= self.ymove
 
         if self.move_down and self.rect.bottom < self.screen_rect.bottom:
             self.centery += self.ymove
-            self.hb_centery += self.ymove
 
         if self.move_left and self.rect.left > 0:
             self.centerx -= self.xmove
-            self.hb_centerx -= self.xmove
 
         if self.move_right and self.rect.right < self.screen_rect.right:
             self.centerx += self.xmove
-            self.hb_centerx += self.xmove
 
+        self.hb_centery = float(self.centery)
+        self.hb_centerx = float(self.centerx + 17)
         self.rect.centery = self.centery
         self.rect.centerx = self.centerx
-        self.hb_rect.centerx = self.hb_centerx
         self.hb_rect.centery = self.hb_centery
+        self.hb_rect.centerx = self.hb_centerx
 
     def update(self):
         self.move()
